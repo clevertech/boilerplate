13bf76b1d54ab8fe1d4927241d5589baed5b58c0
JD Lacey <jd.lacey@clevertech.biz>
2019-02-10 22:15:02 -0400

    Mimic layout of existing boilerplate app

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
 import Books from './components/Books'
 
 const Routes = (props) => (
   <Switch>
-    <Route path="/" exact component={Books} />
+    <Route path="/" exact component={Home} />
+    <Route path="/books" component={Books} />
     <Route component={() => <p>404: page not found</p>} />
   </Switch>
 )
--- frontend/src/components/LayoutShell.js
+++ frontend/src/components/LayoutShell.js
@@ -1,7 +1,74 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const LayoutShell = (props) => (
-  <div>{props.children}</div>
+  <div>
+    <nav className="navbar is-transparent">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <img
+            src="/clevertech.svg"
+            alt="Clevertech React Boilerplate"
+            width="112"
+            height="28"
+          />
+        </Link>
+        <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/">
+            Home
+          </Link>
+          {props.user && (
+            <NavLink className="navbar-item" to="/books">
+              Books
+            </NavLink>
+          )}
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              {props.user && (
+                <p className="control">
+                  <NavLink className="button is-light" to="/account/settings">
+                    <span className="icon">
+                      <i className="fas fa-cogs" />
+                    </span>
+                    <span>Settings</span>
+                  </NavLink>
+                </p>
+              )}
+              {!props.user && (
+                <p className="control">
+                  <NavLink className="button is-primary" to="/account/login">
+                    <span className="icon">
+                      <i className="fas fa-sign-in-alt" />
+                    </span>
+                    <span>Login</span>
+                  </NavLink>
+                </p>
+              )}
+              {props.user && (
+                <p className="control">
+                  <a href="#!" onClick={props.logout} className="navbar-item">
+                    Log Out
+                  </a>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div className="content">{props.children}</div>
+  </div>
 )
 
 export default LayoutShell
