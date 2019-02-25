a1a7c12a46ad876d2536ad2790fc9e7bf303487b
Thomas Trageser <thomast74@gmail.com>
2019-02-25 13:53:12 +0100

    updated initial e2e tests

--- stacks/redux-express/src/tests/e2e/app.test.js
+++ stacks/redux-express/src/tests/e2e/app.test.js
@@ -4,5 +4,5 @@ import { Selector } from 'testcafe';
 fixture`End to end testing`.page`http://localhost:3000`;
 
 test('Home page', async t => {
-  await t.expect(Selector('h1').innerText).eql('Welcome to React');
+  await t.expect(Selector('h1').innerText).eql('Welcome to Clevertech\'s Boilerplate');
 });
