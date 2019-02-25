9bb6a1b586c948b071febbfb7dca9709bdfb3a60
Thomas Trageser <thomast74@gmail.com>
2019-02-25 14:21:11 +0100

    Update error-tracking.js for Sentry/Raven with additional options

--- stacks/redux-express/src/api/src/error-tracking.js
+++ stacks/redux-express/src/api/src/error-tracking.js
@@ -1,6 +1,10 @@
 const Raven = require('raven');
 
 if (process.env.SENTRY_URL) {
-  const raven = new Raven.Client(process.env.SENTRY_URL);
+  const raven = new Raven.Client(process.env.SENTRY_URL, {
+    environment: process.env.NODE_ENV || 'no-env',
+    captureUnhandledRejections: true,
+    autoBreadcrumbs: true,
+  });
   process.on('warning', warning => raven.captureException(warning));
 }
