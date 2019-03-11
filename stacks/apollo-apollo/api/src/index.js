dd4bd2a9874163af411b0cc4070270e3b1f0d7b6
Chris Buryta <chris@buryta.com>
2019-03-11 10:28:21 -0400

    update apollo-apollo stack to have a more mature resolver strucure, yarn packages, readme and resolvers

--- stacks/apollo-apollo/api/src/index.js
+++ stacks/apollo-apollo/api/src/index.js
@@ -7,9 +7,25 @@ const port = +process.env.PORT
 
 require('./error-tracking')
 
+// setup data sources, e.g. knex Models
+const Book = require('./models/Book')
+
 const apolloServerConfig = {
   typeDefs,
-  resolvers
+  resolvers,
+
+  // passed into the context of resolvers
+  dataSources: () => {
+    return {
+      Book
+    }
+  },
+
+  // extract data from the express js request and/or express middleware
+  // context: ({ res }) => ({
+  //   accessToken: res.locals.accessToken || null,
+  //   user: res.locals.user || null
+  // })
 }
 
 const server = new ApolloServer(apolloServerConfig)
--- stacks/apollo-apollo/api/src/schema/index.js
+++ stacks/apollo-apollo/api/src/schema/index.js
@@ -1,11 +1,36 @@
-const { typeDefs: SampleTypeDefs, resolvers: SampleResolvers } = require('./Sample')
+// pull in and build the type definitions and resolvers in this file
+// other files in `/schema` can be limited to app objects/contexts
 
-const typeDefs = [SampleTypeDefs]
+const {
+  typeDefs: GlobalTypeDefs,
+  resolvers: GlobalResolvers
+} = require('./Global')
 
+const {
+  typeDefs: DateTimeTypeDefs,
+  resolvers: DateTimeResolvers
+} = require('./DateTime')
+
+const {
+  typeDefs: SampleTypeDefs,
+  resolvers: SampleResolvers
+} = require('./Sample')
+
+// merge resolvers
 const resolvers = {
+  ...GlobalResolvers,
+  ...DateTimeResolvers,
   ...SampleResolvers
 }
 
+// merge graphql type definitions
+const typeDefs = [
+  GlobalTypeDefs,
+  DateTimeTypeDefs,
+  SampleTypeDefs
+]
+
+// export utils from the schema for use by the ApolloServer config
 module.exports = {
   typeDefs,
   resolvers
