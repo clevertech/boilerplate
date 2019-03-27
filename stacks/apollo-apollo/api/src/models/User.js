65d389fe5adde7a8cb13ec1a306e7481db01ee3c
gamebak <gamebak>
2019-03-27 17:43:22 +0200

    remove unused constants

--- stacks/apollo-apollo/api/src/models/User.js
+++ stacks/apollo-apollo/api/src/models/User.js
@@ -1,10 +1,9 @@
-const { Model } = require('objection')
-const { hashSync, compare } = require('bcrypt')
+const { Model } = require('objection');
 
 class User extends Model {
   static get tableName() {
-    return 'users'
+    return 'users';
   }
 }
 
-module.exports = User
+module.exports = User;
