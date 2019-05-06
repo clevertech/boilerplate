f0f40e1fdef917265bac9d857dedc972186dbf14
Chad <chad@chadfurman.com>
2019-05-06 08:28:45 -0400

    WIP api and frontend and db in docker-compose now

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
-import { dotenv } from "dotenv"
 import express from "express"
 import { postgraphile } from "postgraphile"
 
-dotenv.config()
 const app = express()
 
 app.use(postgrahpile(process.env.DATABASE_URL))
