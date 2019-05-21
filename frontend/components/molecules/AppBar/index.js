a38bd01ebae21e7d8acde64047dc686cbb44920a
Chad <chad@chadfurman.com>
2019-05-20 22:37:10 -0400

    added AppBar molecule

--- frontend/components/molecules/AppBar/index.js
+++ frontend/components/molecules/AppBar/index.js
@@ -1,23 +1,25 @@
 import Link from 'next/link'
 
-const PrimaryNav = () => (
-  <div class="primary-nav">
-    <div class="level">
-      <div class="level-left">
+const AppBar = () => (
+  <div className="primary-nav">
+    <div className="level">
+      <div className="level-left">
         <Logo />
       </div>
-      <div class="level-item has-text-centered">
+      <div className="level-item has-text-centered">
         <Link href="/login"><a>Login</a></Link>
       </div>
-      <div class="level-item has-text-centered">
+      <div className="level-item has-text-centered">
         <Link href="/login"><a>Signup</a></Link>
       </div>
-      <div class="level-item has-text-centered">
+      <div className="level-item has-text-centered">
         <Link href="/login"><a>Reset Password</a></Link>
       </div>
-      <div class="level-item has-text-centered">
+      <div className="level-item has-text-centered">
         <Link href="/login"><a>Homepage</a></Link>
       </div>
     </div>
   </div>
 )
+
+export default AppBar
