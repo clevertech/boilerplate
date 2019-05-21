209883859834ed2bdf9f56686bbacdd27d89d446
Chad <chad@chadfurman.com>
2019-05-20 22:35:46 -0400

    using reakit provider in template for SSR.  Also bringing in Meta like <head> and the primary AppBar into the template for now

--- frontend/templates/MainTemplate/index.js
+++ frontend/templates/MainTemplate/index.js
@@ -1,11 +1,16 @@
-import PrimaryNav from '../../components/PrimaryNav'
+import Meta from '../../components/atoms/Meta'
+import AppBar from '../../components/molecules/AppBar'
+import { Provider } from 'reakit' // we use Provider to make sure SSR is still accessible
 
 import "./index.scss"
 
-const MainTemplate = () => (
-  <div class="main-template">
-    <PrimaryNav />
-  </div>
+const MainTemplate = (...props) => (
+  <Provider>
+    <div className="main-template">
+      <Meta />
+      <AppBar />
+    </div>
+  </Provider>
 )
   
-
+export default MainTemplate
