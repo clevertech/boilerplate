325f9751c6c2919145cf07fce336810eeb07b6d0
Chad <chad@chadfurman.com>
2019-06-02 19:25:44 -0400

    Login Form

---
+++ frontend/components/organisms/LoginForm/index.js
@@ -0,0 +1,12 @@
+import LabeledInput from '../../atoms/LabeledInput'
+import SubmitButton from '../../atoms/SubmitButton'
+
+const LoginForm = () => {
+  return <div className="login-form">
+    <LabeledInput type="text" id="username" label="Username" required="true" />
+    <LabeledInput type="password" id="password" label="Password" required="true" />
+    <SubmitButton />
+  </div>
+}
+
+export default LoginForm
