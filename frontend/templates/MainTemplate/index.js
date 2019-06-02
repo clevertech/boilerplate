a58b1daf0c309a11ef054cffcf0500dfd8c0ac6b
Chad <chad@chadfurman.com>
2019-06-02 18:04:59 -0400

    Added child element support and a fix for regenerator runtime to main template

--- frontend/templates/MainTemplate/index.js
+++ frontend/templates/MainTemplate/index.js
@@ -1,14 +1,16 @@
 import Meta from '../../components/atoms/Meta'
 import AppBar from '../../components/molecules/AppBar'
 import { Provider } from 'reakit' // we use Provider to make sure SSR is still accessible
+import "regenerator-runtime/runtime"
 
 import "./index.scss"
 
-const MainTemplate = (...props) => (
+const MainTemplate = (props) => (
   <Provider>
     <div className="main-template">
       <Meta />
       <AppBar />
+      {props.children}
     </div>
   </Provider>
 )
