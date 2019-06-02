b595db2d85617f1ca735d7e4488a2bdbf24a280d
Chad <chad@chadfurman.com>
2019-06-02 19:25:58 -0400

    Login Page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,5 +1,15 @@
+import MainTemplate from '../templates/MainTemplate'
+import LoginForm from '../components/organisms/LoginForm'
+import LoginLinks from '../components/molecules/LoginLinks'
+import NarrowContainer from '../components/molecules/NarrowContainer'
+
 function Login() {
-  return <div>Login</div>
+  return <MainTemplate appBar="false">
+    <NarrowContainer>
+      <LoginForm />
+      <LoginLinks />
+    </NarrowContainer>
+  </MainTemplate>
 }
 
 export default Login
