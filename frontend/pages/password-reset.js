946453b3ae4313b511d22c010cc57eb0705d491b
Chad <chad@chadfurman.com>
2019-06-02 19:27:15 -0400

    PasswordReset page

--- frontend/pages/password-reset.js
+++ frontend/pages/password-reset.js
@@ -1,5 +1,17 @@
-function PasswordReset() {
-  return <div>Password Reset</div>
+import MainTemplate from '../templates/MainTemplate'
+import PasswordResetForm from '../components/organisms/PasswordResetForm'
+import NarrowContainer from '../components/molecules/NarrowContainer'
+
+const PasswordReset = () => {
+  // todo: retrieve from url params and verify password reset token
+  // todo: shows password reset form only if password reset token is valid
+  // todo: supplies password reset token to password reset form for submission
+  return <MainTemplate appBar="false">
+    <NarrowContainer>
+      <PasswordResetForm/>
+    </NarrowContainer>
+  </MainTemplate>
 }
 
 export default PasswordReset
+
