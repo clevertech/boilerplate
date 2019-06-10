a58e5ded8329b5a47b9ae99fcf98cbe731d37fc3
Chad <chad@chadfurman.com>
2019-06-09 22:28:19 -0400

    effort underway to get API running in dockerfile

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import { postgraphile } from "postgraphile"
 
 const app = express()
 
-app.use(postgrahpile(process.env.DATABASE_URL))
+app.use(postgraphile(process.env.DATABASE_URL))
 
 app.listen(process.env.PORT)
 
