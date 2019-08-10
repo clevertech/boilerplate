55409d48ab859b3a641836c90959b2edcef18c25
Chad Furman <chad@chadfurman.com>
2019-08-10 15:39:06 -0400

    fixing login function parameters to look nicer -- should maybe consider using a rename-hook in postgraphile...

--- db/sqitch/deploy/function_login.sql
+++ db/sqitch/deploy/function_login.sql
@@ -2,14 +2,14 @@
 
 BEGIN;
 
-  create or replace function account.login(_email text, _password text)
+  create or replace function account.login(username text, password text)
   returns account.jwt_token as $$
   declare
     credentials account_private.credentials;
   begin
-    select c.* into credentials from account_private.credentials as c where email = _email;
+    select c.* into credentials from account_private.credentials as c where c.email = username;
 
-    if credentials.password = crypt(_password, credentials.password) then
+    if credentials.password = crypt(password, credentials.password) then
       return ('authenticated', credentials.profile_id)::account.jwt_token;
     else
       return null;
