2cf990259cdc112aee13ee1621e4c8a055272582
Chad Furman <chad@chadfurman.com>
2019-08-10 21:32:02 -0400

    updating redis core test to use mockRedisClient and redisMock

--- api/helpers/redis/core/index.test.js
+++ api/helpers/redis/core/index.test.js
@@ -1,5 +1,6 @@
 import subject from './'
 import redis from 'redis'
+import redisMock, {mockRedisClient} from '../../../__mocks__/redis'
 
 jest.mock('redis')
 
@@ -9,16 +10,16 @@ describe('Redis core helper', () => {
   })
   it('creates and caches a redis client only once', () => {
     subject.connect()
-    expect(redis.createClient).toHaveBeenCalled()
+    expect(redisMock.createClient).toHaveBeenCalled()
     subject.connect()
-    expect(redis.createClient).not.toHaveBeenCalled()
+    expect(redisMock.createClient).not.toHaveBeenCalled()
   })
   it('can disconnect a redis client', () => {
     subject.connect()
-    expect(redis.createClient).toHaveBeenCalled()
+    expect(redisMock.createClient).toHaveBeenCalled()
     subject.disconnect()
     subject.connect()
-    expect(redis.createClient).toHaveBeenCalled()
+    expect(redisMock.createClient).toHaveBeenCalled()
   })
   it('can set a hash in redis', async () => {
     const hashName = 'someHashName'
@@ -26,7 +27,15 @@ describe('Redis core helper', () => {
     const hashProp = 'someHashProp'
 
     await subject.hmset(hashName, hashField, hashProp)
-    expect(redis.createClient).toHaveBeenCalled()
-    expect(redis.hmset).toHaveBeenCalledWith(hashName, hashField, hashProp)
+    expect(redisMock.createClient).toHaveBeenCalled()
+    expect(mockRedisClient.hmset).toHaveBeenCalledWith(hashName, hashField, hashProp)
+  })
+  it('can get a hash from redis', async () => {
+    const hashName = 'someHashName'
+    const hashField = 'someHashField'
+
+    await subject.hmget(hashName, hashField)
+    expect(redisMock.createClient).toHaveBeenCalled()
+    expect(mockRedisClient.hmset).toHaveBeenCalledWith(hashName, hashField)
   })
 })
