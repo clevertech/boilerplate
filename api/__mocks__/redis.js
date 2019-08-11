43c0fa569ec88b897a738c5ec13bbb413092204f
Chad Furman <chad@chadfurman.com>
2019-08-10 21:31:36 -0400

    exporting mockRedisClient directly for easier testing

--- api/__mocks__/redis.js
+++ api/__mocks__/redis.js
@@ -1,23 +1,25 @@
 const redis = jest.genMockFromModule('redis')
 
 const dataStore = {}
+export const mockRedisClient = {
+  hmset: jest.fn((hash,key,value) => {
+    if (datastore[hash]) {
+      datastore[hash][key] = value
+    } else {
+      datastore[hash] = {key: value}
+    }
+  }),
+  hmget: jest.fn((hash,key,value) => {
+    if (datastore[hash]) {
+      return datastore[hash][key]
+    } else {
+      return undefined
+    }
+  })
+}
+
 redis.createClient = function createClient(port, client) {
-  return {
-    hmset: jest.fn((hash,key,value) => {
-      if (datastore[hash]) {
-        datastore[hash][key] = value
-      } else {
-        datastore[hash] = {key: value}
-      }
-    }),
-    hmget: jest.fn((hash,key,value) => {
-      if (datastore[hash]) {
-        return datastore[hash][key]
-      } else {
-        return undefined
-      }
-    })
-  }
+  return mockRedisClient
 }
 
 export default redis
