0ba4a4c06afa10fa151a610ad5aa8a00037a419b
Chad Furman <chad@chadfurman.com>
2019-08-18 03:19:59 -0400

    fixed responseHelper include in postgraphile middleware

--- api/middleware/installPostgraphile.js
+++ api/middleware/installPostgraphile.js
@@ -7,29 +7,29 @@ const db = process.env.DATABASE_URL
 const schemas = postgraphileConfig.POSTGRAPHILE_SCHEMAS
 const options = postgraphileConfig.POSTGRAPHILE_OPTIONS
 
-function installPostgraphile(app, additionalContext) {
-    app.use(postgraphile(db, schemas, {
-      // Import our shared options
-      ...options,
+export const postgraphileOptions = {
+  // Import our shared options
+  ...options,
 
-      // our schema plugins
-      appendPlugins: [
-        LoginJwtHook
-      ],
+  // our schema plugins
+  appendPlugins: [
+    LoginJwtHook
+  ],
 
-      // The return value of this is added to `context` - the third argument of
-      // GraphQL resolvers. This is useful for our custom plugins.
-      additionalGraphQLContextFromRequest(req) {
-        const res = req.res;
-        return {
-          ...additionalContext,
-          res,
-          req,
-          responseHelper(res)
-        }
-      },
-    })
-  )
+  // The return value of this is added to `context` - the third argument of
+  // GraphQL resolvers. This is useful for our custom plugins.
+  additionalGraphQLContextFromRequest(req) {
+    const res = req.res;
+    return {
+      res,
+      req,
+      responseHelper: responseHelper(res)
+    }
+  },
+}
+
+function installPostgraphile(app) {
+  app.use(postgraphile(db, schemas, postgraphileOptions))
 }
 
 export default installPostgraphile
