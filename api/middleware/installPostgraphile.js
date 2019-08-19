4a6526230ab99e2ff96d8d24270399da853c2b03
Chad Furman <chad@chadfurman.com>
2019-08-19 02:31:54 -0400

    made installPostgraphile middleware a bit more modular to help with the test configuration

--- api/middleware/installPostgraphile.js
+++ api/middleware/installPostgraphile.js
@@ -4,28 +4,31 @@ import LoginJwtHook from '../plugins/LoginJwtHook'
 import responseHelper from './postgraphileResponseContext'
 
 const db = process.env.DATABASE_URL
-const schemas = postgraphileConfig.POSTGRAPHILE_SCHEMAS
-const options = postgraphileConfig.POSTGRAPHILE_OPTIONS
+export const schemas = postgraphileConfig.POSTGRAPHILE_SCHEMAS
+export const options = postgraphileConfig.POSTGRAPHILE_OPTIONS
+export const appendPlugins = [
+  LoginJwtHook
+]
 
-export const postgraphileOptions = {
+export const additionalGraphQLContextFromRequest = function additionalGraphQLContextFromRequest(req) {
+  const res = req.res;
+  return {
+    res,
+    req,
+    responseHelper: responseHelper(res)
+  }
+}
+
+const postgraphileOptions = {
   // Import our shared options
   ...options,
 
   // our schema plugins
-  appendPlugins: [
-    LoginJwtHook
-  ],
+  appendPlugins,
 
   // The return value of this is added to `context` - the third argument of
   // GraphQL resolvers. This is useful for our custom plugins.
-  additionalGraphQLContextFromRequest(req) {
-    const res = req.res;
-    return {
-      res,
-      req,
-      responseHelper: responseHelper(res)
-    }
-  },
+  additionalGraphQLContextFromRequest
 }
 
 function installPostgraphile(app) {
