1346d33430d74b799752fe8c63f15a9b0a4f5cd5
Chad Furman <chad@chadfurman.com>
2019-08-19 02:35:19 -0400

    using postgraphileTestHelper in the authHelper for signup and login -- benefits from not having rollback after signup

--- api/integrationTests/testHelpers/authHelper.js
+++ api/integrationTests/testHelpers/authHelper.js
@@ -1,7 +1,4 @@
-import { Client } from 'pg'
-import uuidv4 from 'uuid/v4'
-import request from 'supertest'
-import { app } from '../../../src/app'
+import { setup, teardown, runGraphQLQuery } from "../testHelpers/postgraphileTestHelper"
 
 const getCookie = (cookies, name) => {
   var match = cookies.match(new RegExp('(^| )' + name + '=([^;]+)'))
@@ -10,65 +7,59 @@ const getCookie = (cookies, name) => {
   }
   return null
 }
-export const authenticate = async (email, password, trackingId = uuidv4()) => {
-  const data = {
-    id: 'authenticateMutation',
-    query:
-      'mutation authenticateMutation(\n  $input: AuthenticateInput!\n) {\n  authenticate(input: $input) {\n    clientMutationId\n  }\n}\n',
-    variables: {
-      input: {
-        email,
-        password,
-        trackingId
-      }
+
+export const authenticate = async (email, password) => {
+  await runGraphQLQuery(
+    // GraphQL query goes here:
+    `mutation authenticateMutation(\n  $input: AuthenticateInput!\n) {\n  authenticate(input: $input) {\n    clientMutationId\n  }\n}\n`,
+
+    // GraphQL variables go here:
+    { email, password },
+
+    // Any additional properties you want `req` to have (e.g. if you're using
+    // `pgSettings`) go here:
+    {},
+
+    // This function runs all your test assertions:
+    (json, { pgClient, req }) => {
+      const response = req.res
+      expect(response.statusCode).toBe(200)
+      expect(json.errors).toBeUndefined()
+      expect(json.data.authenticate).toEqual({ clientMutationId: null })
+
+      const cookies = response.get('Set-Cookie').join('; ')
+      const jwt = getCookie(cookies, '__Host-jwt')
+      expect(jwt).toBeTruthy()
+      return res(jwt)
     }
-  }
-  const response = await request(app)
-    .post('/graphql')
-    .send(data)
-
-  expect(response.statusCode).toBe(200)
-  expect(response.body.errors).toBeUndefined()
-  expect(response.body.data.authenticate).toEqual({ clientMutationId: null })
-
-  const cookies = response.header['set-cookie'].join('; ')
-  const jwt = getCookie(cookies, '__Host-jwt')
-  const nonce = getCookie(cookies, '__Host-nonce')
-  expect(jwt).toBeTruthy()
-  expect(nonce).toBeTruthy()
-  return { jwt, nonce }
+  )
 }
 
 /**
  * Create basic test user email: test+uxXT3F68@test.com, password: 1
  * @returns {Promise<void>}
  */
-export const createBasicUser = async ({
-  displayName = 'Test uxXT3F68',
-  email = 'test+uxXT3F68@test.com',
-  password = '1'
+export const signup = async ({
+  displayName,
+  email,
+  password
 } = {}) => {
-  const client = new Client({
-    connectionString
-  })
+  await runGraphQLQuery(
+    // GraphQL query goes here:
+    `mutation signup( $input: SignupInput! ) { profile { id displayName } } }`,
 
-  let res
+    // GraphQL variables go here:
+    { email, password },
 
-  try {
-    await client.connect()
-    const r = await client.query(
-      `insert into account.profile(display_name) values ('${displayName}') returning id`
-    )
-    res = r.rows[0]
-    expect(res.id).toBeTruthy()
-    await client.query(`insert into account_private.credentials(profile_id, email, password, is_verified)
-     values ('${
-       res.id
-     }', '${email.toLowerCase()}', public.crypt('${password}', public.gen_salt('bf')), true)`)
-  } catch (e) {
-    console.error('Error', e.message, e.stack)
-  } finally {
-    await client.end()
-  }
-  return res
+    {},
+    // This function runs all your test assertions:
+    (json, { pgClient, req }) => {
+      const response = req.res
+      expect(response.statusCode).toBe(200)
+      expect(json.errors).toBeUndefined()
+      expect(json.data.signup.profile.id).toBeTruthy()
+      expect(json.data.signup.profile.displayName).toBeTruthy()
+    },
+    false
+  )
 }
