4f15f87279c8ba5d70f1ffa01308b7e7331b22ee
Chad Furman <chad@chadfurman.com>
2019-08-19 02:34:47 -0400

    Setting up integration test for login jwt plugin -- very much WIP still

--- api/integrationTests/plugins/LoginJwtHook.test.js
+++ api/integrationTests/plugins/LoginJwtHook.test.js
@@ -6,36 +6,28 @@ describe('LoginJwtHook Plugin', () => {
   // no need to reinit db due to postgraphileTestHelper's use of transactions
 
   it("should return a JWT", async () => {
+    const testUser = {
+      displayName: 'TestUser',
+      email: 'test@example.com',
+      password: 'someTestPassword'
+    }
     await runGraphQLQuery(
       // GraphQL query goes here:
-      `{ nodeId }`,
+      `mutation { login( $input: LoginInput! ) { clientMutationId jwtToken  } }`,
 
       // GraphQL variables go here:
-      {},
+      {input: {email: testUser.email, password: testUser.password}},
 
       // Any additional properties you want `req` to have (e.g. if you're using
       // `pgSettings`) go here:
-      {
-        // Assuming you're using Passport.js / pgSettings, you could pretend
-        // to be logged in by setting `req.user` to `{id: 17}`:
-        user: { id: 17 },
-      },
+      {},
 
       // This function runs all your test assertions:
-      async (json, { pgClient }) => {
+      (json, { req }) => {
         expect(json.errors).toBeFalsy()
-        expect(json.data.nodeId).toBeTruthy()
-
-        // If you need to, you can query the DB within the context of this
-        // function - e.g. to check that your mutation made the changes you'd
-        // expect.
-        const { rows } = await pgClient.query(
-          `SELECT * FROM app_public.users WHERE id = $1`,
-          [17]
-        )
-        if (rows.length !== 1) {
-          throw new Error("User not found!")
-        }
+        console.log(req.res.get('Set-Cookie'))
+        expect(json.data.clientMutationId).toBeTruthy()
+        expect(json.data.jwtToken).toBeFalsy()
       }
     )
   })
