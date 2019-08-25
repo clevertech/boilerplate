9e0a905c402ade13a50c0e3df7ec59e470d3ce4e
Chad Furman <chad@chadfurman.com>
2019-08-25 19:51:21 -0400

    added parseJwt to jwt helper

--- api/helpers/jwt/index.js
+++ api/helpers/jwt/index.js
@@ -1,17 +1,23 @@
 import jwt from 'jsonwebtoken'
-import ramda from 'ramda'
-//import { redisUtils } from './redis'
 
 function signJwt(rawJwt) {
   const jwtSignOptions = {
     expiresIn: '1 day'
   }
   return jwt.sign(rawJwt, process.env.JWT_SECRET, jwtSignOptions)
-  // const profileId = rawPayload ? rawPayload.profile_id : null
+}
 
-  // redisUtils.removeKeyFromHash('jwt', profileId)
-  // redisUtils.setKeyInHash('jwt', profileId, newJwt)
+function parseJwt(rawJwt) {
+  try {
+    return jwt.verify(rawJwt, process.env.JWT_SECRET)
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
 
 
-export { signJwt }
+export {
+  signJwt,
+  parseJwt
+}
--- api/helpers/jwt/index.test.js
+++ api/helpers/jwt/index.test.js
@@ -1,9 +1,30 @@
 import jwt from 'jsonwebtoken'
-import { signJwt } from './'
-jest.mock('jsonwebtoken')
+import { signJwt, parseJwt } from './'
+
+const signSpy = jest.spyOn(jwt, 'sign')
+const verifySpy = jest.spyOn(jwt, 'verify')
+const rawJwt = {'someString': 'someValue'}
+
 describe('Jwt Helper', () => {
+  afterEach(jest.clearAllMocks)
   it('signs jwts', () => {
-    signJwt('someJwt')
-    expect(jwt.sign).toHaveBeenCalled()
+    const signedJwt = signJwt(rawJwt)
+    expect(signSpy).toHaveBeenCalled()
+    expect(signedJwt).toEqual(expect.any(String))
+  })
+  it('decodes valid jwts', () => {
+    const validJwt = jwt.sign(rawJwt, process.env.JWT_SECRET)
+    expect(() => {
+      const parsedJwt = parseJwt(validJwt)
+      expect(verifySpy).toHaveBeenCalled()
+      expect(parsedJwt).toEqual(expect.objectContaining(rawJwt))
+    }).not.toThrow()
+  })
+  it('does not decode invalid jwts', () => {
+    const invalidJwt = jwt.sign(rawJwt, "badkey")
+    expect(() => {
+      expect(parseJwt(invalidJwt)).toEqual(null)
+      expect(verifySpy).toHaveBeenCalled()
+    }).not.toThrow()
   })
 })
