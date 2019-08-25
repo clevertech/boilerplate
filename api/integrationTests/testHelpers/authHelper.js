59a55a5b4dc468d11bfc4b9450430a1ea7905a61
Chad Furman <chad@chadfurman.com>
2019-08-24 22:23:20 -0400

    removing authenticate helper since it's not correct and not used yet -- yagni

--- api/integrationTests/testHelpers/authHelper.js
+++ api/integrationTests/testHelpers/authHelper.js
@@ -10,33 +10,6 @@ const getCookie = (cookies, name) => {
   return null
 }
 
-export const authenticate = async (email, password) => {
-  await runGraphQLQuery(
-    // GraphQL query goes here:
-    `mutation authenticateMutation(\n  $input: AuthenticateInput!\n) {\n  authenticate(input: $input) {\n    clientMutationId\n  }\n}\n`,
-
-    // GraphQL variables go here:
-    { email, password },
-
-    // Any additional properties you want `req` to have (e.g. if you're using
-    // `pgSettings`) go here:
-    {},
-
-    // This function runs all your test assertions:
-    (json, { pgClient, req }) => {
-      const response = req.res
-      expect(response.statusCode).toBe(200)
-      expect(json.errors).toBeUndefined()
-      expect(json.data.authenticate).toEqual({ clientMutationId: null })
-
-      const cookies = response.get('Set-Cookie').join('; ')
-      const jwt = getCookie(cookies, '__Host-jwt')
-      expect(jwt).toBeTruthy()
-      return res(jwt)
-    }
-  )
-}
-
 /**
  * Create basic test user email: test+uxXT3F68@test.com, password: 1
  * @returns {Promise<void>}
