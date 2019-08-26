dc6ad4f68b213c34ad51b62bab2a3a4b55bd5d4f
Chad Furman <chad@chadfurman.com>
2019-08-25 22:49:07 -0400

    WIP SignupEmailVerification plugin -- todo tests added

---
+++ api/plugins/SignupEmailVerification/index.test.js
@@ -0,0 +1,7 @@
+
+describe("SignupEmailVerification plugin", () => {
+  test.todo("hooks the root signup mutation")
+  test.todo("executes database signup procedure")
+  test.todo("stores email verification token in redis with a 1 hour expiration")
+  test.todo("sends email with verification link including verification token")
+})
